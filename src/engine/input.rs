80f09046c44014d9cf38d6fd052eab4568d9469d
phn0me <phn0me@metrognomes.dev>
2026-06-24 22:46:35 +0200

    starting with inputhandling

--- src/engine/input.rs
+++ src/engine/input.rs
@@ -1,1 +1,30 @@
+use winit::event::ElementState;
+#[allow(dead_code)]
+pub struct InputState {
+    pub keys_down: [bool; 256],
+    pub keys_just_pressed: [bool; 256],
+}
 
+#[allow(dead_code)]
+impl InputState {
+    pub fn update(&mut self, key_code: winit::keyboard::KeyCode, state: ElementState) {
+        let idx = key_code as usize;
+        if idx >= 256 {
+            return;
+        } // Safety clamp
+
+        match state {
+            ElementState::Pressed => {
+                self.keys_down[idx] = true;
+                self.keys_just_pressed[idx] = true;
+            }
+            ElementState::Released => {
+                self.keys_down[idx] = false;
+            }
+        }
+    }
+
+    pub fn reset_just_pressed(&mut self) {
+        self.keys_just_pressed.fill(false);
+    }
+}
