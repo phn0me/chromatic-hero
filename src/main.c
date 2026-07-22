0ed96717531d1fe02b327d2d948c4590eac244c8
phn0me <phn0me@metrognomes.dev>
2026-07-22 21:01:23 +0200

    migrating to cmake to have clion behave even better

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
-#include <SDL3/SDL_main.h>
 #include "game.h"
+
+#include <SDL3/SDL_main.h>
+
 int main(int argc, char* argv[]) {
     (void) argc;
     (void) argv;
 
+    Engine engine;
+    Game game;
     return 0;
 }
